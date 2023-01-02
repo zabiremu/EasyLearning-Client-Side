e995963b7b69bfaf841c33d5318b965d65ea118b
zabiremu <zabirraihan@gmail.com>
2023-01-02 23:25:11 +0600

    top-banner part-2

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,11 +1,18 @@
 import React, { Component, Fragment } from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 // import '../../assets/css/style.css'
 export default class TopBanner extends Component {
   render() {
     return (
       <Fragment >
         <Container fluid className="top-banner">
+            <Row className=''>
+                <Col className='top-content text-center'>
+                    <h1>Easy Learning</h1>
+                    <h4>Learn Profesionally</h4>
+                    <button className='btn btn-primary'>Learn More</button>
+                </Col>
+            </Row>
         </Container>
       </Fragment>
     )

8dd6eabf812e9c6d91e74066da4385c538661bb7
zabiremu <zabirraihan@gmail.com>
2023-01-02 23:10:34 +0600

    Top-Banner part-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './assets/css/bootstrap.min.css'
+import './assets/css/style.css'
+import TopBanner from './components/TopBanner/TopBanner';
 function App() {
   return (
     <div>
-    
+      <TopBanner/>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,13 @@
+import React, { Component, Fragment } from 'react'
+import { Container } from 'react-bootstrap'
+// import '../../assets/css/style.css'
+export default class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment >
+        <Container fluid className="top-banner">
+        </Container>
+      </Fragment>
+    )
+  }
+}

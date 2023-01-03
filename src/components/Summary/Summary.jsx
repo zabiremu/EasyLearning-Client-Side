d288ed56f2671910f4517267aca6eaf09d15b6af
zabiremu <zabirraihan@gmail.com>
2023-01-03 20:50:15 +0600

    Summary Section Part-02

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -5,18 +5,24 @@ export default class Summary extends Component {
     return (
       <Fragment>
         <Container fluid className="summary p-0">
-          <Container>
+          <Container className="text-center">
             <Row>
               <Col lg={8} md={12} sm={12}>
-                <Row>
+                <Row className="Summary_wrapper">
                   <Col lg={4} md={4} sm={4}>
-                    One
+                    <h1 className="summary_title">35000</h1>
+                    <h4 className="summary_details">Students Worldwide</h4>
+                    <hr className="text-light w-25 mx-auto" style={{ height:'3px' }} />
                   </Col>
                   <Col lg={4} md={4} sm={4}>
-                    Two
+                    <h1 className="summary_title">22</h1>
+                    <h4 className="summary_details">Course Published</h4>
+                    <hr className="text-light w-25 mx-auto" style={{ height:'3px' }} />
                   </Col>
                   <Col lg={4} md={4} sm={4}>
-                    Three
+                    <h1 className="summary_title">3000</h1>
+                    <h4 className="summary_details">Students Reviews</h4>
+                    <hr className="text-light w-25 mx-auto" style={{ height:'3px' }} />
                   </Col>
                 </Row>
               </Col>

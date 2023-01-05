7fd91a42ebe7a0b3c979e97d4ed4027dacd55ec3
zabiremu <zabirraihan@gmail.com>
2023-01-05 15:30:28 +0600

    Videos Section Part-02

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,7 +1,23 @@
 import React, { Component, Fragment } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Modal, Button } from "react-bootstrap";
 
 export default class Videos extends Component {
+  constructor() {
+    super();
+    this.state = {
+      show: false,
+    };
+  }
+  handleClose = () => {
+    this.setState({
+      show: false,
+    });
+  };
+  handleShow = () => {
+    this.setState({
+      show: true,
+    });
+  };
   render() {
     return (
       <Fragment>
@@ -10,30 +26,49 @@ export default class Videos extends Component {
           <div className="bottom"></div>
           <Row>
             <Col lg={6} md={6} sm={12}>
-              <div className="service-desc">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
-              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
-              fugiat iusto fuga praesentium optio, eaque rerum! Provident
-              similique accusantium nemo autem.
-              <br/><br/>
-              Veritatis obcaecati tenetur iure
-              eius earum ut molestias architecto voluptate aliquam nihil,
-              eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-              tenetur error, harum nesciunt ipsum debitis quas aliquid.
-              Reprehenderit, quia. Quo neque error repudiandae fuga? Ipsa
-              laudantium molestias eos sapiente officiis modi at sunt excepturi
-              expedita sint?
-              <br/><br/>
-              Sed quibusdam recusandae alias error harum maxime
-              adipisci amet laborum. Perspiciatis minima nesciunt dolorem!
+              <div className="service-desc mt-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+                <br />
+                <br />
+                Veritatis obcaecati tenetur iure eius earum ut molestias
+                architecto voluptate aliquam nihil, eveniet aliquid culpa
+                officia aut! Impedit sit sunt quaerat, odit, tenetur error,
+                harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia.
+                Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+                sapiente officiis modi at sunt excepturi expedita sint?
+                <br />
+                <br />
+                Sed quibusdam recusandae alias error harum maxime adipisci amet
+                laborum. Perspiciatis minima nesciunt dolorem!
               </div>
             </Col>
             <Col lg={6} md={6} sm={12}>
-              Video
+              <div className="videoCard text-center">
+                <span className="sum_icon" onClick={this.handleShow}>
+                  <i class="fa-solid fa-video-slash"></i>
+                </span>
+              </div>
             </Col>
           </Row>
         </Container>
+        <Modal size="lg" show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </Fragment>
     );
   }
